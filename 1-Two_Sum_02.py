5c0e94a3bf8d80b20219087e668b5848b108d4f4
Kaique Gonzaga Silvestre <kaique.silvestre.22@gmail.com>
2025-02-28 20:54:40 -0300

    Learning the codes of git and changing things to learn how to do it

--- 1-Two_Sum_02.py
+++ 1-Two_Sum_02.py
@@ -1,6 +1,14 @@
+"""
+Given an array of integers "nums" and an integer "target", return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+"""
+# After studying the exercise i have learned that that's the right answer to it. Grat Logic, Beautiful.
 
 def TwoSum(target, array):
-    # dict{NumbertoSum: PositionOfTheLockedOne}
+    # dict{NumbertoSum: IndexOfTheNumber}
     IndexDict = dict()
     for index, num in enumerate(array):
         if num in IndexDict:
@@ -10,9 +18,6 @@ def TwoSum(target, array):
     return "There's no such sum that's equal to the target"
 
 
-
-
 l1 = [2, 2, 7, 15, 7, 11, 7]
-
 
 print(TwoSum(9, l1))
